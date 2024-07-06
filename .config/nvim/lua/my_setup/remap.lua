dfecb28a1815ab81ce3dcd4ef98d2bb8ec75ed70
Sean Lin <seanlinshiang@gmail.com>
2024-07-06 12:23:47 +0800

    add system clipboard paste keybinding and change other keymap

--- .config/nvim/lua/my_setup/remap.lua
+++ .config/nvim/lua/my_setup/remap.lua
@@ -1,11 +1,13 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
 
 vim.keymap.set("n", "<leader>t", vim.cmd.Neotree)
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
