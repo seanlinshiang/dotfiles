cb8ef443c86dd6bde14c4f360186752be1836e25
Sean Lin <seanlinshiang@gmail.com>
2024-08-05 11:36:57 +0800

    Nvim: Add new shortcuts and update lazy packages

--- .config/nvim/lua/my_setup/remap.lua
+++ .config/nvim/lua/my_setup/remap.lua
@@ -17,6 +17,11 @@ vim.keymap.set("n", "<leader>O", "O<Esc>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-o>", "<C-o>zz");
+vim.keymap.set("n", "<C-i>", "<C-i>zz");
+vim.keymap.set("n", "<C-d>", "<C-d>zz");
+vim.keymap.set("n", "<C-u>", "<C-u>zz");
 -- vim.keymap.set("n", "<leader>j", function ()
 --     vim.cmd("sp | term") 
 -- end)
