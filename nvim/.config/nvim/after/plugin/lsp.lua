8ffe7741e89333c149c333a90ea0be2a13e5bf62
Sean Lin <seanlinshiang@gmail.com>
2025-09-17 00:00:13 +0800

    Nvim: update all lazy packages and fix some lsp issue

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -35,7 +35,7 @@ end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
-      'tsserver',
+      'ts_ls',
       'rust_analyzer',
       'pyright',
       'eslint'
